6eeb9e7ec4569637ccde7dbde360182068898826
eduardo3450 <126858749+eduardo3450@users.noreply.github.com>
2023-03-03 04:48:01 -0300

    first commit
    commit inicial

---
+++ assets/js/style.js
@@ -0,0 +1,51 @@
+/*------------ SHOW MENU-------------*/
+const showMneu = (togglemId, navId) => {
+    const toggle = document.getElementById(togglemId),
+        nav = document.getElementById(navId)
+    /*valida se se existe as variaveis*/
+    if (toggle && nav) {
+        /*aqui adiciono a class show-menu na div com a class nav__menu*/
+        toggle.addEventListener('click', () => {
+            /*adicina a class show-menu na div que tem o id nav-menu */
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMneu('nav-togglem', 'nav')
+
+/*------------ REMOVER MENU MOBILE-------------*/
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction() {
+    const navMenu = document.getElementById('nav')
+    /*Quando um link for clicado a class show-menu será removido*/
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== DEIXA O LINK CLICADO COM A CLASS ACTIVE-LINK ====================*/
+const linkColor = document.querySelectorAll('.nav__item')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(L => L.classList.remove('active-link'))
+        this.classList.add('active-link')
+    }
+}
+
+linkColor.forEach(L=> L.addEventListener('click', colorLink))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
